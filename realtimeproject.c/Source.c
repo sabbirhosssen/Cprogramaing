22209debd1b82092ffe4750acf2938fb9bb5d256
@sabbirhosssen <sabbirhossen58sh58@gmail.com>
2022-11-13 23:32:17 +0600

    Add project files.

---
+++ realtimeproject.c/Source.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main() {
+	
+	// this programming problem is 15-20 don't show. but run the programming show all number
+	int i;
+
+	for (i = 1; i <=20; i++) {
+		if (i == 16) {
+			continue;
+		}
+		else if (i==17) {
+			continue;
+		}
+		else if (i == 18) {
+			continue;
+		}
+		else if (i == 19) {
+			continue;
+		}
+		
+		
+		printf("%d\n", i);
+	}
+	
+}
